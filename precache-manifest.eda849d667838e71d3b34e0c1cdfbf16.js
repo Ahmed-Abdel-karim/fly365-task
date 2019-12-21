d7c5b29b1919a08de574336ef2ae92207c0a8a39
ahmed <ahmed.abdelkarim@nozol.com>
2019-12-21 21:37:23 +0200

    Updates

---
+++ precache-manifest.eda849d667838e71d3b34e0c1cdfbf16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca229e5f4a90f80a4a84055cc4131b2c",
+    "url": "/fly365-task/index.html"
+  },
+  {
+    "revision": "b53d5327dbcd234f0653",
+    "url": "/fly365-task/static/css/main.67dc9062.chunk.css"
+  },
+  {
+    "revision": "ed79d2892c2c97899b34",
+    "url": "/fly365-task/static/js/2.0984a57c.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/fly365-task/static/js/2.0984a57c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b53d5327dbcd234f0653",
+    "url": "/fly365-task/static/js/main.eab7c192.chunk.js"
+  },
+  {
+    "revision": "dda07b10de2ca3f7bde1",
+    "url": "/fly365-task/static/js/runtime-main.f827e6e2.js"
+  }
+]);
